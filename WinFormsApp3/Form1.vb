1deb075ebe50b1630319fcdd53cc56425f08397d
FelixDAT19 <61045804+FelixDAT19@users.noreply.github.com>
2022-03-01 10:34:31 +0200

    mer ändringar av namnet

--- WinFormsApp3/Form1.vb
+++ WinFormsApp3/Form1.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Multiplikation.Click
+    Private Sub Multiplikation_Click(sender As Object, e As EventArgs) Handles Multiplikation.Click
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles InputBox.TextChanged
+    Private Sub InputBox_TextChanged(sender As Object, e As EventArgs) Handles InputBox.TextChanged
 
     End Sub
 End Class
