4de2a9c297df1e0e92b6ad64e8465ace355c3971
FelixDAT19 <61045804+FelixDAT19@users.noreply.github.com>
2022-03-01 10:31:15 +0200

    en text ruta och en label skapad

--- WinFormsApp3/Form1.Designer.vb
+++ WinFormsApp3/Form1.Designer.vb
@@ -22,10 +22,40 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Multiplikation = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(48, 143)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(279, 39)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Multiplikation
+        '
+        Me.Multiplikation.AutoSize = True
+        Me.Multiplikation.Location = New System.Drawing.Point(48, 69)
+        Me.Multiplikation.Name = "Multiplikation"
+        Me.Multiplikation.Size = New System.Drawing.Size(70, 32)
+        Me.Multiplikation.TabIndex = 1
+        Me.Multiplikation.Text = "Multi"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Multiplikation)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Multiplikation As Label
 End Class
